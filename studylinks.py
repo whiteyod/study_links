b43c93123280848d6f4b565658a406db88819b99
Rostyslav Popov <79105768+whiteyod@users.noreply.github.com>
2021-03-24 14:44:08 +0200

    Update studylinks.py

--- studylinks.py
+++ studylinks.py
@@ -7,25 +7,12 @@ import asyncio
 import random as rand
 
 
-# Объект бота
+
 bot = Bot(token="1555033388:AAEbf1sTNZdp-JuhhiKFmOjE_4bLkKHLEks")
-# Диспетчер для бота
+
 dp = Dispatcher(bot)
-# Включаем логирование, чтобы не пропустить важные сообщения
+
 logging.basicConfig(level=logging.INFO)
-
-
-
-
-@dp.errors_handler(exception=BotBlocked)
-async def error_bot_blocked(update: types.Update, exception: BotBlocked):
-    # Update: объект события от Telegram. Exception: объект исключения
-    # Здесь можно как-то обработать блокировку, например, удалить пользователя из БД
-    print(f"Меня заблокировал пользователь!\nСообщение: {update}\nОшибка: {exception}")
-
-    # Такой хэндлер должен всегда возвращать True,
-    # если дальнейшая обработка не требуется.
-    return True
 
 
 
@@ -43,12 +30,6 @@ async def cmd_inline_url(message: types.Message):
     await message.answer("Учение-хуение", reply_markup=keyboard)
 
 
-
-@dp.message_handler(commands="random")
-async def cmd_random(message: types.Message):
-    keyboard = types.InlineKeyboardMarkup()
-    keyboard.add(types.InlineKeyboardButton(text="Нажми меня", callback_data="random_value"))
-    await message.answer("Нажмите на кнопку, чтобы бот отправил число от 1 до 10", reply_markup=keyboard)
 
 
 
